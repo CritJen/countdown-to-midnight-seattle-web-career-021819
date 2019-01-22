f88d5a22bcf7a17dc7e2f05e3b8d303d4cad89b0
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-22 03:15:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,10 +4,10 @@ def countdown (number)
   while number > 0
     puts "#{number} SECOND(S)!"
     number -= 1
-    countdown_with_sleep
   end
   "HAPPY NEW YEAR!"
 end
+
 def countdown_with_sleep
   sleep(1.minute)
 end
